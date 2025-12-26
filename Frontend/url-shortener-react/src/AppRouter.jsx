dcff26466e09d67923fb862fd55d99a8a3cc0b3f
shivamguptaa708 <165505550+shivamguptaa708@users.noreply.github.com>
2025-12-26 22:08:45 +0530

    Update AppRouter.jsx

--- Frontend/url-shortener-react/src/AppRouter.jsx
+++ Frontend/url-shortener-react/src/AppRouter.jsx
@@ -34,10 +34,3 @@ const AppRouter = () => {
 
 export default AppRouter;
 
-export const SubDomainRouter = () => {
-    return(
-         <Routes>
-         <Route path="/:url" element={<ShortenUrlPage/>}/>
-         </Routes>
-    )
-}
