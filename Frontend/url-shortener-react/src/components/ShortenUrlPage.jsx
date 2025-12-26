eaf1cb15b6e21965c58b97fa616b149281952fd8
shivamguptaa708 <165505550+shivamguptaa708@users.noreply.github.com>
2025-12-26 21:55:02 +0530

    Update ShortenUrlPage.jsx

--- Frontend/url-shortener-react/src/components/ShortenUrlPage.jsx
+++ Frontend/url-shortener-react/src/components/ShortenUrlPage.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ShortenUrlPage = () => {
-    const { url } = useParams();
+  const { url } = useParams();
 
   useEffect(() => {
-   if(url){
-    window.location.href = import.meta.env.VITE_BACKEND_URL + `/${url}`;
-   }
+    if (url) {
+      window.location.replace(
+        `${import.meta.env.VITE_BACKEND_URL}/r/${url}`
+      );
+    }
   }, [url]);
-  return null;
-}
 
-export default ShortenUrlPage
+  return null;
+};
+
+export default ShortenUrlPage;
